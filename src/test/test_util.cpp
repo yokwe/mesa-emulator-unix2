330491aa66d91c1d5384149fe91938c8fae52383
hasegawa <hasegawa@macmini2020.lan>
2023-03-12 18:46:57 +0900

    Add more test cases.

--- src/test/test_util.cpp
+++ src/test/test_util.cpp
@@ -37,35 +37,101 @@ static const auto logger = util::Logger::getLogger("test_util");
 
 #include "test_base.h"
 
+#include "../util/format.h"
+#include "../util/mesa.h"
 #include "../util/util.h"
 
 class test_util : public test_base {
 	CPPUNIT_TEST_SUITE(test_util);
 
+	// format.h
+	CPPUNIT_TEST(test_snprintf);
+	CPPUNIT_TEST(test_sprintf);
+	// mesa.h
+	CPPUNIT_TEST(test_toIntMesaNumber);
+	// util.h
 	CPPUNIT_TEST(test_demangle);
 	CPPUNIT_TEST(test_startsWith);
 	CPPUNIT_TEST(test_endsWith);
+	CPPUNIT_TEST(test_toHexString);
 
 	CPPUNIT_TEST_SUITE_END();
 
 public:
+	// format.h
+	void test_snprintf() {
+		{
+			std::string a = "ab  ";
+			std::string b;
+			util::snprintf(b, 2, "%-4s", "ab");
+			CPPUNIT_ASSERT_EQUAL(a, b);
+		}
+		{
+			std::string a = "ab  ";
+			std::string b;
+			util::snprintf(b, 10, "%-4s", "ab");
+			CPPUNIT_ASSERT_EQUAL(a, b);
+		}
+		{
+			std::string a = "ab  ";
+			std::string b;
+			util::snprintf(b, 10, "%-4s", std::string("ab"));
+			CPPUNIT_ASSERT_EQUAL(a, b);
+		}
+		{
+			std::string a = "ab     3";
+			//               12341234
+			std::string b;
+			util::snprintf(b, 10, "%-4s%4d", "ab", 3);
+			CPPUNIT_ASSERT_EQUAL(a, b);
+		}
+	}
+	void test_sprintf() {
+		{
+			std::string a = "ab  ";
+			std::string b = util::sprintf("%-4s", "ab");
+			CPPUNIT_ASSERT_EQUAL(a, b);
+		}
+		{
+			std::string a = "ab  ";
+			std::string b = util::sprintf("%-4s", std::string("ab"));
+			CPPUNIT_ASSERT_EQUAL(a, b);
+		}
+		{
+			std::string a = "ab     3";
+			//               12341234
+			std::string b = util::sprintf("%-4s%4d", "ab", 3);
+			CPPUNIT_ASSERT_EQUAL(a, b);
+		}
+	}
+	// mesa.h
+	void test_toIntMesaNumber() {
+		CPPUNIT_ASSERT_EQUAL(10, util::toIntMesaNumber("0x0A"));
+		CPPUNIT_ASSERT_EQUAL(10, util::toIntMesaNumber("012"));
+		CPPUNIT_ASSERT_EQUAL(10, util::toIntMesaNumber("AH"));
+		CPPUNIT_ASSERT_EQUAL(10, util::toIntMesaNumber("12B"));
+	}
+	// util.h
 	void test_demangle() {
-		auto a = typeid(std::vector<int>).name();
-		auto b = util::demangle(a);
-		auto c = util::demangle(typeid(std::vector<int>).name());
-
-		logger.info("a = %s!", a);
-		logger.info("b = %s!", b);
-		logger.info("c = %s!", c);
-    	CPPUNIT_ASSERT_EQUAL(b, c);
+		enum AA {aa};
+		auto a = util::demangle(typeid(AA).name());
+		auto b = std::string("test_util::test_demangle()::AA");
+    	CPPUNIT_ASSERT_EQUAL(a, b);
 	}
 	void test_startsWith() {
+		(void)logger;
 		CPPUNIT_ASSERT_EQUAL(true,  util::startsWith("aabbcc", "aa"));
 		CPPUNIT_ASSERT_EQUAL(false, util::startsWith("aabbcc", "aaa"));
 	}
 	void test_endsWith() {
 		CPPUNIT_ASSERT_EQUAL(true,  util::endsWith("aabbcc", "cc"));
 		CPPUNIT_ASSERT_EQUAL(false, util::endsWith("aabbcc", "ccc"));
+	}
+	void test_toHexString() {
+		const uint8_t data[] = "AB";
+		auto a = util::toHexString(2, data);
+		auto b = std::string("4142");
+    	CPPUNIT_ASSERT_EQUAL(a, b);
 	}
 };
 
